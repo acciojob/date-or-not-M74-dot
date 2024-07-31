425e278e0a0b1d2b39eb9a0bb7f4e2453c2d94ab
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-31 18:29:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 var isDate = function (input) {
-  //   write your code here
+  // write your code here
+	if(input isinstanceof Date){
+		return !isNaN(input.getTime());
+	}
+	if(typeof input === 'string' || typeof input === 'number'){
+		const parseDate = new Date(input);
+		return !isNaN(input.getTime())
+	}
+	return false;
 };
+
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
+
